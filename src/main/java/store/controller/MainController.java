8095bcc360532a8a76d28a3e08f459dcb076b451
SISU <101076058+SISUinSea@users.noreply.github.com>
2024-11-06 10:23:06 +0000

    refactor: 메소드 합치기

--- src/main/java/store/controller/MainController.java
+++ src/main/java/store/controller/MainController.java
@@ -20,22 +20,23 @@ public class MainController {
     public static void run() throws IOException {
         ProductTable productTable = bootProductTable();
         printProductTable(productTable);
-        PromotionTable promotionTable = bootPromotionTable();
-        String purchaseLine = askPurchaseRequest();
 
+        String purchaseLine = askPurchaseRequest();
         PurchaseRequests purchaseRequests = createPurchaseRequestQueue(purchaseLine);
         productTable.validateRequests(purchaseRequests.getRequests());
-
+        
+        
     }
 
     public static ProductTable bootProductTable() throws IOException {
+        bootPromotionTable();
         List<String> productLines = Files.readAllLines(Paths.get("./src/main/resources/products.md"));
         List<String> trimmedProductLines = removeHeader(productLines);
         
         return createProductTable(trimmedProductLines);
     }
 
-    public static PromotionTable bootPromotionTable() throws IOException {
+    private static PromotionTable bootPromotionTable() throws IOException {
         List<String> promotionLines = Files.readAllLines(Paths.get("./src/main/resources/promotions.md"));
         List<String> trimmedPromotionLines = removeHeader(promotionLines);
         return createPromotionTable(trimmedPromotionLines);
