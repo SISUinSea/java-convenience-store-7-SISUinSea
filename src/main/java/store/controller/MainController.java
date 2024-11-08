ad0ffc530902e06f4a4f85535b8d5cbd5a7e777e
SISU <101076058+SISUinSea@users.noreply.github.com>
2024-11-08 14:03:22 +0000

    fix: 멤버십 할인 올바른 형식을 입력받을 때까지 질문하도록 수정

--- src/main/java/store/controller/MainController.java
+++ src/main/java/store/controller/MainController.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import store.model.Discount.Discount;
 import store.model.Product.ProductTable;
 import store.model.Transaction.TransactionTable;
+import store.utils.ErrorMessage;
 import store.model.PurchaseRequest.PurchaseRequests;
 import store.model.Receipt.Receipt;
 
@@ -17,6 +18,7 @@ import static store.view.OutputView.printProductTable;
 import static store.view.OutputView.printReceipt;
 import static store.view.InputView.askApplyMembershipDiscount;
 import static store.view.InputView.askToContinuePurchase;
+import static store.view.InputView.askUntilGetValidAnswer;
 
 public class MainController {
     public static void run() throws IOException {
@@ -44,11 +46,15 @@ public class MainController {
     }
 
     public static Discount suggestMembershipDiscount() {
-        String answer = askApplyMembershipDiscount();
-        if (answer.equals("Y")) {
-            return createMembershipDiscount();
-        }
-        return null;
+        return (Discount) askUntilGetValidAnswer(() -> {
+            String answer = askApplyMembershipDiscount();
+            if (answer.equals("Y")) {
+                return createMembershipDiscount();
+            }
+            if (answer.equals("N")) {
+                return null;
+            }
+            throw new IllegalArgumentException(ErrorMessage.ILLEGAL_INPUT.getDescription());
+        });
     }
-
 }
