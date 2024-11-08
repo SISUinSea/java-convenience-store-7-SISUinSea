ddddb9398780146c1cc0bdf5ed5bf1035ad11bf6
SISU <101076058+SISUinSea@users.noreply.github.com>
2024-11-08 10:43:23 +0000

    feat: 구매 요청 입력 검증 기능 추가

--- src/main/java/store/utils/Parser.java
+++ src/main/java/store/utils/Parser.java
@@ -1,5 +1,6 @@
 package store.utils;
 
+import java.util.Arrays;
 import java.util.List;
 
 public class Parser {
@@ -16,9 +17,36 @@ public class Parser {
     }
 
     public static String removeSquareBrakets(String wrappedString) {
-        if(!(wrappedString.startsWith("[") && wrappedString.endsWith("]"))) {
+        return wrappedString.substring(1, wrappedString.length() - 1);
+    }
+
+    public static String validateRequestsSyntax(String requestsLine) {
+        if (requestsLine.contains("--") || requestsLine.contains(",,")) {
             throw new IllegalArgumentException();
         }
-        return wrappedString.substring(1, wrappedString.length() - 1);
+        Arrays.stream(requestsLine.split(",", -1)).forEach(Parser::validateSingleRequestSyntax);
+        return requestsLine;
+    }
+
+    private static void validateSingleRequestSyntax(String requestLine) {
+        if (requestLine == null) {
+            throw new IllegalArgumentException();
+        }
+        if (!requestLine.startsWith("[") || !requestLine.endsWith("]")) {
+            throw new IllegalArgumentException();
+        }
+        if (!requestLine.contains("-")) {
+            throw new IllegalArgumentException();
+        }
+        requestLine = removeSquareBrakets(requestLine);
+        String[] splittedRequestLine = requestLine.split("-", -1);
+        if (splittedRequestLine.length != 2) {
+            throw new IllegalArgumentException();
+        }
+        for (String string : splittedRequestLine) {
+            if (string.length() == 0 || string == null) {
+                throw new IllegalArgumentException();
+            }
+        }
     }
 }
