8940ac8556a6ab71835254c108ece3fb0f11c6ee
SISU <101076058+SISUinSea@users.noreply.github.com>
2024-11-08 11:43:42 +0000

    fix: 프로모션 중인 모든 상품이 프로모션 상품, 일반 상품을 가지도록 수정

--- src/main/java/store/model/Product/ProductFactory.java
+++ src/main/java/store/model/Product/ProductFactory.java
@@ -3,12 +3,14 @@ package store.model.Product;
 import static store.utils.Parser.parsePromotionName;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
 
 public class ProductFactory {
     public static ProductTable createProductTable(List<String> productTableData) {
         List<Product> table = productTableData.stream().map(ProductFactory::createSingleProduct).toList();
-        return new ProductTable(table);
+        List<Product> filledTable = fillGeneralProductIfAbsent(table);
+        return new ProductTable(filledTable);
     }
 
     private static Product createSingleProduct(String productData) {
@@ -18,6 +20,28 @@ public class ProductFactory {
         Integer quantity = Integer.parseInt(parsedProductData[2]);
         String promotion = parsePromotionName(parsedProductData[3]);
         return new Product(name, price, quantity, promotion);
+    }
+
+    private static List<Product> fillGeneralProductIfAbsent(List<Product> table) {
+        List<Product> filledTable = new ArrayList<>();
+        for (Product product : table) {
+            filledTable.add(product);
+            if (!hasDoubleProducts(product.getName(), table)) {
+                filledTable.add(createEmptyProduct(product.getName(), product.getPrice()));
+            }
+        }
+        return filledTable;
+    }
+
+    private static Product createEmptyProduct(String name, Integer price) {
+        return new Product(name, price, 0, "null");
+    }
+
+    private static boolean hasDoubleProducts(String productName, List<Product> table) {
+        if (table.stream().filter(product -> product.getName().equals(productName)).count() == 2) {
+            return true;
+        }
+        return false;
     }
 
     public static Product createSingleTransactionProduct(String name, Integer totalQuantity,
