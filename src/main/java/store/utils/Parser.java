0898895dcdcabe1150fc53ef6a7794d73129e76b
SISU <101076058+SISUinSea@users.noreply.github.com>
2024-11-09 07:07:17 +0000

    feat, refactor: 구매 요청 입력 검증 메소드 분리, 검증 추가
    - 같은 물품을 두 번 주문했을 때(예: [콜라-1],[콜라-2] 예외 발생시키는 기능 추가
    - 요청 수량이 0이거나 음수일 때 예외 발생시키는 기능 추가

--- src/main/java/store/utils/Parser.java
+++ src/main/java/store/utils/Parser.java
@@ -1,6 +1,7 @@
 package store.utils;
 
 import java.util.Arrays;
+import java.util.HashSet;
 import java.util.List;
 
 public class Parser {
@@ -25,28 +26,75 @@ public class Parser {
             throw new IllegalArgumentException(ErrorMessage.ILLEGAL_REQUEST_SYNTAX.getDescription());
         }
         Arrays.stream(requestsLine.split(",", -1)).forEach(Parser::validateSingleRequestSyntax);
+        validateNoDuplicateProductRequests(requestsLine.split(",", -1));
         return requestsLine;
     }
 
     private static void validateSingleRequestSyntax(String requestLine) {
+        validateRequestNotNull(requestLine);
+        validateRequestHasSquareBracket(requestLine);
+        validateRequestHasDash(requestLine);
+        requestLine = removeSquareBrakets(requestLine);
+        String[] splittedRequestLine = requestLine.split("-", -1);
+        validateRequestsHaveOnlyNameAndQuantity(splittedRequestLine);
+        validateParsedRequestsNotNull(splittedRequestLine);
+        validateRequestQuantityIsNotZero(splittedRequestLine);
+    }
+
+    private static void validateRequestNotNull(String requestLine) {
         if (requestLine == null) {
             throw new IllegalArgumentException(ErrorMessage.ILLEGAL_REQUEST_SYNTAX.getDescription());
         }
+    }
+
+    private static void validateRequestHasSquareBracket(String requestLine) {
         if (!requestLine.startsWith("[") || !requestLine.endsWith("]")) {
             throw new IllegalArgumentException(ErrorMessage.ILLEGAL_REQUEST_SYNTAX.getDescription());
         }
+    }
+
+    private static void validateRequestHasDash(String requestLine) {
         if (!requestLine.contains("-")) {
             throw new IllegalArgumentException(ErrorMessage.ILLEGAL_REQUEST_SYNTAX.getDescription());
         }
-        requestLine = removeSquareBrakets(requestLine);
-        String[] splittedRequestLine = requestLine.split("-", -1);
+    }
+
+    private static void validateRequestsHaveOnlyNameAndQuantity(String[] splittedRequestLine) {
         if (splittedRequestLine.length != 2) {
             throw new IllegalArgumentException(ErrorMessage.ILLEGAL_REQUEST_SYNTAX.getDescription());
         }
+    }
+
+    private static void validateParsedRequestsNotNull(String[] splittedRequestLine) {
         for (String string : splittedRequestLine) {
             if (string.length() == 0 || string == null) {
                 throw new IllegalArgumentException(ErrorMessage.ILLEGAL_REQUEST_SYNTAX.getDescription());
             }
+        }
+    }
+
+    private static void validateNoDuplicateProductRequests(String[] splittedRequestLine) {
+        List<String> productNames = Arrays.stream(splittedRequestLine).map(s -> s.split("-")[0]).toList();
+        if (splittedRequestLine.length != new HashSet<String>(productNames).size()) {
+            throw new IllegalArgumentException(ErrorMessage.ILLEGAL_INPUT.getDescription());
+        }
+    }
+
+    private static void validateRequestQuantityIsNotZero(String[] splittedRequestLine) {
+        Integer quantity;
+        try {
+            quantity = Integer.parseInt(splittedRequestLine[1]);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException(ErrorMessage.ILLEGAL_INPUT.getDescription());
+        }
+        if (quantity <= 0) {
+            throw new IllegalArgumentException(ErrorMessage.ILLEGAL_INPUT.getDescription());
+        }
+    }
+
+    public static void validateYNAnswer(String answer) {
+        if (!answer.equals("Y") && !answer.equals("N")) {
+            throw new IllegalArgumentException(ErrorMessage.ILLEGAL_INPUT.getDescription());
         }
     }
 }
